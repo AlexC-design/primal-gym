5f096fb18b5d65c5a659b9f8d1319367e84941ae
AlexC-Design <alex94.design@gmail.com>
2019-11-24 17:43:40 +0000

    added cta button and navbar

--- src/components/0-LandingPage/LandingPage.js
+++ src/components/0-LandingPage/LandingPage.js
@@ -2,13 +2,16 @@ import React from "react";
 import Logo from "./Logo/Logo";
 import "../css/style.css";
 import Logotype from "./Logotype/Logotype";
+import Navbar from "./Navbar/Navbar";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
+      <Navbar />
       <Logo />
       <Logotype />
       <h1>Where fitness comes naturally</h1>
+      <button className="btn-join-main">Join Now</button>
     </div>
   );
 };
---
+++ src/components/0-LandingPage/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../../css/style.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="flex-eq"></div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About us</a>
+        </li>
+        <li>
+          <a href="#">Classes</a>
+        </li>
+        <li>
+          <a href="#">Trainers</a>
+        </li>
+        <li>
+          <a href="#">Pricing</a>
+        </li>
+      </ul>
+      <button className="btn-login">Log In</button>
+    </div>
+  );
+};
+
+export default Navbar;
