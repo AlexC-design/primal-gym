14163f09dd3c861382259119352f8ff5093c4a15
AlexC-Design <alex94.design@gmail.com>
2020-01-03 12:41:40 +0000

    added mobile menu functionality

--- src/components/~re-usables/MobileMenu/MenuList/MenuList.js
+++ src/components/~re-usables/MobileMenu/MenuList/MenuList.js
@@ -4,8 +4,26 @@ import "./menu-list.css";
 export default class MenuList extends Component {
   render() {
     return (
-      <div>
-        <div></div>
+      <div className={`menu-list ${this.props.menuState}`}>
+        <a onClick={this.props.changeMenuState} href="#home">
+          Home
+        </a>
+
+        <a onClick={this.props.changeMenuState} href="#about">
+          About us
+        </a>
+
+        <a onClick={this.props.changeMenuState} href="#classes">
+          Classes
+        </a>
+
+        <a onClick={this.props.changeMenuState} href="#trainers">
+          Trainers
+        </a>
+
+        <a onClick={this.props.changeMenuState} href="#pricing">
+          Pricing
+        </a>
       </div>
     );
   }
--- src/components/~re-usables/MobileMenu/MobileMenu.js
+++ src/components/~re-usables/MobileMenu/MobileMenu.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 import "./mobile-menu.css";
 import MenuButton from "./MenuButton/MenuButton";
+import MenuList from "./MenuList/MenuList";
 
 export default class MobileMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { menuState: "closed" };
+  }
+
+  changeMenuState = () => {
+    if (this.state.menuState === "closed") {
+      this.setState({ menuState: "open" });
+    } else {
+      this.setState({ menuState: "closed" });
+    }
+  };
   render() {
-    return <MenuButton scroll={this.props.scroll} />;
+    return (
+      <>
+        <div onClick={this.changeMenuState}>
+          <MenuButton
+            scroll={this.props.scroll}
+            buttonState={this.state.menuState}
+          />
+        </div>
+        <MenuList
+          menuState={this.state.menuState}
+          changeMenuState={this.changeMenuState}
+        />
+      </>
+    );
   }
 }
