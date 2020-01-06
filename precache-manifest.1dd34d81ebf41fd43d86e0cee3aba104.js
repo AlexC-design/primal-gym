117e51cc43348fc39ce3e3373f79635e584bc985
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-01-06 09:40:39 +0000

    Updates

---
+++ precache-manifest.1dd34d81ebf41fd43d86e0cee3aba104.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288fc37b8af3e21b7c69db38864b3bde",
+    "url": "/primal-gym/index.html"
+  },
+  {
+    "revision": "cbee27b92981e71597c7",
+    "url": "/primal-gym/static/css/main.af61948a.chunk.css"
+  },
+  {
+    "revision": "a94dc43c82ba7c696e51",
+    "url": "/primal-gym/static/js/2.bff4aace.chunk.js"
+  },
+  {
+    "revision": "cbee27b92981e71597c7",
+    "url": "/primal-gym/static/js/main.ab9e5678.chunk.js"
+  },
+  {
+    "revision": "8d924a5934d3033404e7",
+    "url": "/primal-gym/static/js/runtime-main.5f79a621.js"
+  },
+  {
+    "revision": "2f1e547193cc11015dab330a004fba97",
+    "url": "/primal-gym/static/media/about-us-bg.2f1e5471.jpg"
+  },
+  {
+    "revision": "8b0ca964d345a0098a766c9804fcf665",
+    "url": "/primal-gym/static/media/arrow-body-vertical.8b0ca964.png"
+  },
+  {
+    "revision": "d8ac37e1db453477ffba5baf3e5c114f",
+    "url": "/primal-gym/static/media/arrow-body.d8ac37e1.png"
+  },
+  {
+    "revision": "49547a2a71c0088777865db27f8756b4",
+    "url": "/primal-gym/static/media/class-men-debris.49547a2a.png"
+  },
+  {
+    "revision": "efef242269b8a3fbf4ff5a38f43f10bc",
+    "url": "/primal-gym/static/media/class-men.efef2422.png"
+  },
+  {
+    "revision": "941323e13d014e86629a25f8613b9921",
+    "url": "/primal-gym/static/media/class-women-debris.941323e1.png"
+  },
+  {
+    "revision": "37919f2588e08db9414938bcf003a665",
+    "url": "/primal-gym/static/media/class-women.37919f25.png"
+  },
+  {
+    "revision": "f5a7c4f3fbcc79f6e6f6709611f864d1",
+    "url": "/primal-gym/static/media/class1.f5a7c4f3.png"
+  },
+  {
+    "revision": "eb30bdb24dadb3a3bcbce7d241d527c8",
+    "url": "/primal-gym/static/media/class2.eb30bdb2.png"
+  },
+  {
+    "revision": "5a0cab8887f556eed37a80e858959e5e",
+    "url": "/primal-gym/static/media/class3.5a0cab88.png"
+  },
+  {
+    "revision": "1126276c7367dd00a4bc336f63d8d381",
+    "url": "/primal-gym/static/media/class4.1126276c.png"
+  },
+  {
+    "revision": "c2fc2ad9bdb4401c80fe12f8fe5f0f28",
+    "url": "/primal-gym/static/media/classes-bg.c2fc2ad9.jpg"
+  },
+  {
+    "revision": "e3945e98e4c660673822e6a2f6b9f9b2",
+    "url": "/primal-gym/static/media/home-icon.e3945e98.svg"
+  },
+  {
+    "revision": "84b525ef9d5f4cc3677bd55769a6c76f",
+    "url": "/primal-gym/static/media/icon1.84b525ef.svg"
+  },
+  {
+    "revision": "cffdb919500f0f8ebbcf46fa4c2eab6b",
+    "url": "/primal-gym/static/media/icon2.cffdb919.svg"
+  },
+  {
+    "revision": "6d4480895e865a2a8b453a50f4863863",
+    "url": "/primal-gym/static/media/icon3.6d448089.svg"
+  },
+  {
+    "revision": "d05a668951698a012e61eafc217c9ce1",
+    "url": "/primal-gym/static/media/icon4.d05a6689.svg"
+  },
+  {
+    "revision": "9a314c5ac1889e4856042e65cb529d02",
+    "url": "/primal-gym/static/media/icon5.9a314c5a.svg"
+  },
+  {
+    "revision": "e225ffc29b4eadd68a3ab597bc74fa26",
+    "url": "/primal-gym/static/media/icon6.e225ffc2.svg"
+  },
+  {
+    "revision": "4647be63e71e5e091b57f6c9f437833e",
+    "url": "/primal-gym/static/media/landing-bg.4647be63.jpg"
+  },
+  {
+    "revision": "eea2116833144226972897aa3a7c105e",
+    "url": "/primal-gym/static/media/logo.eea21168.png"
+  },
+  {
+    "revision": "683da98d9715e46e840c2893864b3d95",
+    "url": "/primal-gym/static/media/page-icon.683da98d.svg"
+  },
+  {
+    "revision": "59f8ac42303c18874213875a9a2c15a2",
+    "url": "/primal-gym/static/media/trainer1.59f8ac42.png"
+  },
+  {
+    "revision": "9286f9991a5f58d9095530619fc8a261",
+    "url": "/primal-gym/static/media/trainer2.9286f999.png"
+  },
+  {
+    "revision": "66353724e2478214c633607e73109d20",
+    "url": "/primal-gym/static/media/trainer3.66353724.png"
+  },
+  {
+    "revision": "06318ed30276c01b65b694eb3ca5d3ce",
+    "url": "/primal-gym/static/media/user-icon.06318ed3.svg"
+  }
+]);
